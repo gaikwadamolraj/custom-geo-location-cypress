48089850c3d5b8e2891ada8206e85c0e05ebdfad
Amol Gaikwad <amol_gaikwad@apple.com>
2022-03-22 14:35:19 +0000

    Used package

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,5 @@
 describe('Custom Geo location params', () => {
     it('Get the custom geo location', () => {
-      cy.visitWithMockGeo('./test.html', 51.507351, -0.127758)
+      cy.visitWithCustomGeoLoc('./test.html', 51.507351, -0.127758)
     })
   })
